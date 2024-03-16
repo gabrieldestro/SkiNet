852141efbe2af0bce4ed1129fc05d586140c7476
gdestro <gabrielaugustodestro@gmail.com>
2024-03-16 15:34:27 -0300

    development of the list page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/Product';
-import { Pagination } from './models/Pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +8,11 @@ import { Pagination } from './models/Pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
-
+  
   constructor(private http: HttpClient) {
 
   } 
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => {
-        console.log('request completed.');
-      }
-    })
   }
 }
