c54dab274f9d71a0e2a188859cd93d9191b246d6
gdestro <gabrielaugustodestro@gmail.com>
2024-03-23 13:42:48 -0300

    adding redis

--- API/Extensions/AplicationServicesExtensions.cs
+++ API/Extensions/AplicationServicesExtensions.cs
@@ -8,6 +8,7 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -26,7 +27,14 @@ namespace API.Extensions
                 }
             );
 
+            services.AddSingleton<IConnectionMultiplexer>(c => 
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ApiBehaviorOptions>(options =>
